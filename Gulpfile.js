a7b6c17c42a0b2d1a4a009e44751291e18ad2d27
Izel Nakri <contact@izelnakri.com>
2016-11-24 14:08:45 +0100

    js basic structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,34 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
+      sourcemaps = require('gulp-sourcemaps'),
+      uglify = require('gulp-uglify'),
+      size = require('gulp-size'),
       concat = require('gulp-concat');
 
-gulp.task('default', () => {
-  console.log('default task runs');
+gulp.task('js', () => {
+  gulp.src([
+    'frontend/js/vendor/jquery.js',
+    'frontend/js/vendor/tether.js',
+    'frontend/js/vendor/lodash.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'frontend/js/application.js',
+    'frontend/js/components/**/*.js'
+  ]).pipe(concat('application.js'))
+    .pipe(size())
+    .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('scss', () => {
   gulp.src('frontend/scss/application.scss')
+      .pipe(sourcemaps.init())
       .pipe(sass({ outputStyle: 'compressed' }))
       .pipe(concat('application.css'))
+      .pipe(size())
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('public/css'));
 });
 
-
-
-gulp.task('watch', ['scss'], () => {
+gulp.task('default', ['scss', 'js'], () => {
   gulp.watch('frontend/scss/**/*.scss', ['scss']);
+  gulp.watch('frontend/js/**/*.js', ['js']);
 });
---
+++ frontend/js/application.js
@@ -0,0 +1,5 @@
+var result = _.map([1, 2, 3, 4, 5], (x) => {
+  return x * 2;
+});
+
+console.log(result);
